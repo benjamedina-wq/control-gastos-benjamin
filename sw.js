59b92e3edaa5c496a1a36ad1d4de003c94a931cd
Benjamin Medina <benjamedina@gmail.com>
2026-06-04 00:48:05 -0300

    Evitar cache viejo en GitHub Pages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "control-gastos-v45";
+const CACHE_NAME = "control-gastos-v46";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -28,16 +28,12 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-
-      return fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match("./index.html"));
-    })
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
   );
 });
